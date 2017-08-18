8b9c6ca87259feaf83050bc92318f2242df04f0a
deeptechs <deeptechs@users.noreply.github.com>
2017-08-18 22:30:06 +0300

    Assigment 9 and 10

---
+++ python-data-structures-Coursera/Assigment 10.2.py
@@ -0,0 +1,33 @@
+#10.2 Write a program to read through the mbox-short.txt and figure out the distribution by hour of the day for each of the messages.
+# You can pull the hour out from the 'From ' line by finding the time and then splitting the string a second time using a colon.
+#From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+#Once you have accumulated the counts for each hour, print out the counts, sorted by hour as shown below.
+
+name = raw_input("Enter file:")
+if len(name) < 1: name = "mbox-short.txt"
+handle = open(name)
+
+hours = dict()
+for line in handle:
+    if line.startswith('From '):
+        words = line.split(':')
+        st = words[0]
+        st = st.split()
+        hour = st[len(st)-1]
+        hours[hour] = hours.get(hour, 0) + 1
+
+# Sort By Key
+hours = sorted(hours.items())
+for i, j in hours:
+    print(i, j)
+
+# Sort by Value
+# lst = list()
+# for i, j in hours.items():
+#     newtup = j, i
+#     lst.append(newtup)
+#
+# lst = sorted(lst)
+# for i, j in lst:
+#     print(i, j)
+
---
+++ python-data-structures-Coursera/Assigment 9.4.py
@@ -0,0 +1,26 @@
+# 9.4 Write a program to read through the mbox-short.txt and figure out who has the sent the greatest number of mail messages.
+# The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail.
+# The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file.
+# After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
+
+name = raw_input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+
+my_dict = dict()
+for line in handle:
+    word = line.split()
+    if len(word) > 0:
+        for i in range(len(word)):
+            if word[i] == 'From' and len(word) > i:
+                my_dict[word[i + 1]] = my_dict.get(word[i + 1], 0) + 1
+
+max_sent_val = 0
+for key, value in my_dict.items():
+    if my_dict[key] >= max_sent_val:
+        max_sent_val = my_dict[key]
+        max_sent_key = key
+
+print(max_sent_key, my_dict[max_sent_key])
